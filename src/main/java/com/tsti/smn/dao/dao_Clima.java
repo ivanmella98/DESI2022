1ddf6155a6da6731557e87895a789774ab84d054
talit <talit@DESKTOP-1KOH4B1>
2022-12-04 20:56:28 -0300

    ctrlshifto

--- src/main/java/com/tsti/smn/dao/dao_Clima.java
+++ src/main/java/com/tsti/smn/dao/dao_Clima.java
@@ -1,13 +1,9 @@
 package com.tsti.smn.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tsti.smn.entidades.Clima;
-import com.tsti.smn.entidades.clima_extendido;
 
 @Repository
 public interface dao_Clima extends JpaRepository<Clima,Integer>{
